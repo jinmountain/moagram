1351aea50e8ed9e80cd58f1084d3bc3ef0426fbc
jinmountain <lvoemountain@gmail.com>
2019-05-08 02:01:51 -0500

    /new bug

--- routes/contents.js
+++ routes/contents.js
@@ -473,28 +473,29 @@ router.post("/:id", function (req, res, next) {
 });
 
 router.get("/new", middleware.authCheck, function(req, res){
-    var paste = clipboardy.readSync();
-    var videoParse = urlParser.parse(paste);
+    // var paste = clipboardy.readSync();
+    // var videoParse = urlParser.parse(paste);
 
-    var pst = "";
-    var pvd = "";
+    // var pst = "";
+    // var pvd = "";
     
-    if(paste != undefined){
-        if(videoParse != undefined){
-            pst = paste;
-            pvd = videoParse.provider;
-        } else {
-            pst = paste;
-            pvd = ""
-        }
-    } else {
-        pst = "";
-        pvd = "";
-    }
-    res.render(req.user.lang + "/contents/new", {
-        paste: pst,
-        provider: pvd 
-    });
+    // if(paste != undefined){
+    //     if(videoParse != undefined){
+    //         pst = paste;
+    //         pvd = videoParse.provider;
+    //     } else {
+    //         pst = paste;
+    //         pvd = ""
+    //     }
+    // } else {
+    //     pst = "";
+    //     pvd = "";
+    // }
+    // res.render(req.user.lang + "/contents/new", {
+    //     paste: pst,
+    //     provider: pvd 
+    // });
+    res.render(req.user.lang + "/contents/new");
 });
 
 router.get("/:id", middleware.authCheck, function(req, res, next){
